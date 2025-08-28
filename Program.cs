1ec6628d4c40892392b37a5bfd511a0d52d7c0fa
Victor Velez <victor.velez@inexto.com>
2025-08-27 21:47:28 -0500

    feat: Create y Read en /weatherforecast con lista en memoria y swagger habilitado

--- Program.cs
+++ Program.cs
@@ -1,5 +1,3 @@
-using Proyecto.DespliegueSoftware.Models;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,7 +8,6 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
@@ -23,26 +20,35 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-var products = new List<Product>();
+var forecasts = new List<WeatherForecast>();
 
-app.MapPost("/products", (Product input) =>
+app.MapPost("/weatherforecast", (WeatherForecast input) =>
 {
-     var created = new Product(products.Count + 1, input.Name, input.Price);
-     products.Add(created);
-     return Results.Created($"/products/{created.Id}", created);
+     forecasts.Add(input);
+     return Results.Created($"/weatherforecast/{input.Date:yyyy-MM-dd}", input);
 });
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+     if (forecasts.Count > 0)
+          return Results.Ok(forecasts);
+
+     var summaries = new[]
+     {
+        "Freezing","Bracing","Chilly","Cool","Mild","Warm",
+        "Balmy","Hot","Sweltering","Scorching"
+    };
+
+     var forecast = Enumerable.Range(1, 5).Select(index =>
+         new WeatherForecast
+         (
+             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+             Random.Shared.Next(-20, 55),
+             summaries[Random.Shared.Next(summaries.Length)]
+         ))
+         .ToArray();
+
+     return Results.Ok(forecast);
 })
 .WithName("GetWeatherForecast")
 .WithOpenApi();
